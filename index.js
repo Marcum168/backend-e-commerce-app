9a63bed0556b8ef586d8e8e0f96e1b6c7b0fe35e
Rachel Gulliver <rachelgulliver971@gmail.com>
2021-04-07 10:07:59 -0400

    so far

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const products = [
+  {
+    name: 
+  }
+]
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -9,3 +14,18 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.post('/', (req,res) => {
+  res.send('Is this post thing working or not?')
+})
+
+app.use(function(req,res,next){
+
+  res.header('Access-Control-Allow-Origin', '*');
+
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+
+  //First thing database is going to be full of, is products with image URL's and a title, and a price, description, categories
+})
