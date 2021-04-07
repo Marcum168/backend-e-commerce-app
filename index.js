6e03932867527c2459c4ed29abeb3d143c9efdcc
Rachel Gulliver <rachelgulliver971@gmail.com>
2021-04-07 10:19:39 -0400

    The basics

--- index.js
+++ index.js
@@ -4,9 +4,149 @@ const port = 3000;
 
 const products = [
   {
-    name: 
+    name: 'Hisense 4.4-cu ft Freestanding Mini Fridge Freezer Compartment (Black Stainless Steel)',
+    imgurl: 'https://mobileimages.lowes.com/product/converted/819130/819130027166.jpg?size=pdhi',
+    category: 'Kitchen Appliances',
+    price: 239.00,
+    description: 'This energy star 4.4 Cu. Ft. Double door black stainless steel look mini fridge is perfect for college dorms, break rooms, or just extra storage for your home and office space. Separate 1.1 cu. Ft. freezer provides storage room for ice cream and frozen meals. The adjustable legs and reversible door design allow for placement of this mini refrigerator virtually anywhere you need it. The sleek and fashionable design does not compromise its functionality. This model has convenient storage shelves on the refrigerator door for 2-Liter bottles as well as extra space for single-serve yogurt or canned beverages. Removable glass shelves provide storage for larger items to efficiently utilize interior capacity. The sliding crisper drawer makes it easy to store your fresh fruits and vegetables.'
+  },
+  {
+    name: 'Insignia™ - 0.7 Cu. Ft. Compact Microwave - Black',
+    imgurl:'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6315/6315750_sd.jpg',
+    category: '',
+    price: '',
+    description:
   }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+  {
+    name:,
+    imgurl:,
+    category:,
+    price:,
+    description:
+  }
+
 ]
+
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -29,3 +169,5 @@ app.use(function(req,res,next){
 
   //First thing database is going to be full of, is products with image URL's and a title, and a price, description, categories
 })
+
+app.get
